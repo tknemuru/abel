9f9433b7695ddafea3abede24970c0e22aca8842
tknemuru <kojikoji.max.5252@gmail.com>
2019-12-22 22:10:17 +0900

    add surveyor

---
+++ src/services/Identity-surveyor.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = {
+  /**
+   * @description 素性の調査を行います。
+   * @returns {void}
+   */
+  survey () {
+    // 調査対象のカラムを取得
+    // テンプレートのSQLを取得
+    // テンプレートからSQLを組み立てる
+    // パラメータを作成
+    // SQLを実行
+    return null
+  }
+}
